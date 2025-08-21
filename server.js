ce69b7a065a38e0fd03c7c4eda6a894daa6edec3
Hussain Ahmed <84622365+hussainmohd03@users.noreply.github.com>
2025-08-21 08:41:21 +0300

    add import routes in server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express')
 require('dotenv').config()
 const path = require('path')
 
-
 // Initialize app
 const app = express()
 
@@ -16,8 +15,6 @@ const port = process.env.PORT ? process.env.PORT : 3000
 // Require MiddleWares
 const morgan = require('morgan')
 
-
-
 // use MiddleWares
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
@@ -25,20 +22,24 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(cors())
 app.use(express.json())
 
-
 // Root Route
 app.get('/', (req, res) => {
   res.send('Your app is connected . . . ')
 })
 
 // Require Routers
-
+const auctionRT = require('./routes/Auctions')
+const biddingsRT = require('./routes/Biddings')
+const itemsRT = require('./routes/items')
+const userRT = require('./routes/User')
 
 // use Routers
-
+app.use('/auctions', auctionRT)
+app.use('/biddigns', biddingsRT)
+app.use('/items', itemsRT)
+app.use('/auth', userRT)
 
 // Listener
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
 })
-
