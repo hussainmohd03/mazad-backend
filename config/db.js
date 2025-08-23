e71e6e1384ba9659d286ca6cc25e8907fe3547e0
mad6717 <mad6717@gmail.com>
2025-08-23 17:46:24 +0300

    db.js and env setup

--- config/db.js
+++ config/db.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO_URI)
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log('MongoDB connected ╰(*°▽°*)╯')
+  } catch (err) {
+    console.error(err.message)
+    process.exit(1)
+  }
+}
 
-mongoose.connection.on('connected', () => {
-  console.log(`connected to Database: ${mongoose.connection.name}`)
-})
-
-module.exports = mongoose
+module.exports = connectDB
