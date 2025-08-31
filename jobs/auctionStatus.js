31fe66f81e95e26f9d4eebbcc052ce3ac433dca7
mad6717 <mad6717@gmail.com>
2025-08-31 12:51:23 +0300

    fixing a commit

--- jobs/auctionStatus.js
+++ jobs/auctionStatus.js
@@ -6,54 +6,59 @@ const User = require('../models/user')
 const nowUTC = () => new Date()
 
 const checkAuctions = async () => {
-  // //TODO 1: Get all upcoming auctions with startData <= now and change their status to ongoing
-  // const upcoming = await Auction.find({
-  //   status: 'upcoming',
-  //   startDate: { $lte: nowUTC() }
-  // })
-  // for (let auction of upcoming) {
-  //   auction.status = 'ongoing'
-  //   await auction.save()
-  // }
-  // //TODO 3: get all ongoing auctions with endDate <= now and change their status to closed,
-  // const expired = await Auction.find({
-  //   status: 'ongoing',
-  //   endDate: { $lte: nowUTC() }
-  // })
-  // for (let auction of expired) {
-  //   auction.status = 'closed'
-  //   //TODO 4:  get highest bid and set winningBidID,
-  //   const highest_bid = await Bidding.findOne({ auctionId: auction._id }).sort({
-  //     amount: -1
-  //   })
-  //   // TODO 6:  user Balance
-  //   if (highest_bid) {
-  //     const highest_bidder = await User.findById(highest_bid.userId)
-  //     console.log(highest_bidder)
-  //     console.log(highest_bid.userId)
-  //     // TODO 6.1: check user balance
-  //     if (highest_bidder.balance >= auction.currentPrice) {
-  //       // TODO 6.2 : if sufficient reduce balance
-  //       highest_bidder.balance -= auction.currentPrice
-  //     }
-  //     // TODO 7: Trigger transaction
-  //     auction.winningBid = highest_bid._id
-  //     await auction.save()
-  //     await highest_bidder.save()
-  //     await Transaction.create({
-  //       sellerId: auction.ownerId,
-  //       buyerId: highest_bid.userId,
-  //       price: highest_bid.amount,
-  //       itemId: auction.itemId,
-  //       date: nowUTC()
-  //     })
-  //   }
-  // }
-  // // TODO 5: emit only ongoing auctions
-  // const ongoingAuctions = await Auction.find({ status: 'ongoing' }).populate(
-  //   'itemId'
-  // )
-  // global.io.emit('updateAuctions', { ongoing: ongoingAuctions })
+  //TODO 1: Get all upcoming auctions with startData <= now and change their status to ongoing
+  const upcoming = await Auction.find({
+    status: 'upcoming',
+    startDate: { $lte: nowUTC() }
+  })
+
+  for (let auction of upcoming) {
+    auction.status = 'ongoing'
+    await auction.save()
+  }
+
+  //TODO 3: get all ongoing auctions with endDate <= now and change their status to closed,
+  const expired = await Auction.find({
+    status: 'ongoing',
+    endDate: { $lte: nowUTC() }
+  })
+  for (let auction of expired) {
+    auction.status = 'closed'
+
+    //TODO 4:  get highest bid and set winningBidID,
+    const highest_bid = await Bidding.findOne({ auctionId: auction._id }).sort({
+      amount: -1
+    })
+
+    // TODO 6:  user Balance
+    if (highest_bid) {
+      const highest_bidder = await User.findById(highest_bid.userId)
+      console.log(highest_bidder)
+      console.log(highest_bid.userId)
+      // TODO 6.1: check user balance
+      if (highest_bidder.balance >= auction.currentPrice) {
+        // TODO 6.2 : if sufficient reduce balance
+        highest_bidder.balance -= auction.currentPrice
+      }
+      // TODO 7: Trigger transaction
+      auction.winningBid = highest_bid._id
+      await auction.save()
+      await highest_bidder.save()
+      await Transaction.create({
+        sellerId: auction.ownerId,
+        buyerId: highest_bid.userId,
+        price: highest_bid.amount,
+        itemId: auction.itemId,
+        date: nowUTC()
+      })
+    }
+  }
+  // TODO 5: emit only ongoing auctions
+  const ongoingAuctions = await Auction.find({ status: 'ongoing' }).populate(
+    'itemId'
+  )
+
+  global.io.emit('updateAuctions', { ongoing: ongoingAuctions })
 }
 
 module.exports = checkAuctions
