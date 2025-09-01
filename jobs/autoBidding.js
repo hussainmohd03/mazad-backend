874135ffb1059fa7bafe36c052cc1f77db3a7e6c
Hussain Ahmed <84622365+hussainmohd03@users.noreply.github.com>
2025-09-01 22:56:55 +0300

    fix auto bid job issue

--- jobs/autoBidding.js
+++ jobs/autoBidding.js
@@ -1,117 +1,218 @@
-const auction = require('../models/auction')
-const Auction = require('../models/auction')
-const Bidding = require('../models/Bidding')
-const Transaction = require('../models/Transaction')
+const Autobidding = require('../models/Autobidding')
 const User = require('../models/user')
-const Watchlist = require('../models/Watchlist')
-
+const Bidding = require('../models/Bidding')
+const Auction = require('../models/auction')
 const nowUTC = () => new Date()
 
-const checkAuctions = async () => {
-  let newNotfication = ''
-  //TODO 1: Get all upcoming auctions with startData <= now and change their status to ongoing
-  const upcoming = await Auction.find({
-    status: 'upcoming',
-    startDate: { $lte: nowUTC() }
-  })
-
-  for (let auction of upcoming) {
-    auction.status = 'ongoing'
-    await auction.save()
-  }
-
-  //TODO 3: get all ongoing auctions with endDate <= now and change their status to closed,
-  const expired = await Auction.find({
-    status: 'ongoing',
-    endDate: { $lte: nowUTC() }
-  })
-
-  for (let auction of expired) {
-    auction.status = 'closed'
-    const watchlist = await Watchlist.find({ auctionId: auction._id }).populate(
-      ['userId', 'auctionId']
-    )
-
-    if (watchlist.length !== 0) {
-      for (let i = 0; i < watchlist.length; i++) {
-        newNotfication = await User.findByIdAndUpdate(
-          watchlist[i].userId,
-          {
-            $push: {
-              notifications: {
-                message: `#${watchlist[i].auctionId._id} has been sold.`
-              }
-            }
-          },
-          { new: true }
-        )
-
-        newNotfication =
-          newNotfication.notifications[newNotfication.notifications.length - 1]
-            .message
-        // console.log(watchlist[i].userId._id.toString())
-        global.io
-          .to(watchlist[i].userId._id.toString())
-          .emit('notify', newNotfication)
-        console.log('from job', newNotfication)
-        // console.log(watchlist[i].userId._id)
-
-        // update watchlist entity by removing the records with auctions that have been closed
-        await Watchlist.findByIdAndDelete(watchlist[i]._id.toString())
-      }
-    }
-
-    //TODO 4:  get highest bid and set winningBidID,
-    const highest_bid = await Bidding.findOne({ auctionId: auction._id }).sort({
-      amount: -1
+const makeAutoBidding = async () => {
+  const auctions = await Auction.find({ status: 'ongoing' })
+  for (let i = 0; i < auctions.length; i++) {
+    const highestBidder = await Bidding.find({ auctionId: auctions[i] }).sort({
+      createdAt: -1
     })
-    // TODO 6:  user Balance
-    if (highest_bid) {
-      const highest_bidder = await User.findById(highest_bid.userId)
-      // console.log(highest_bidder)
-      // console.log(highest_bid.userId)
-      // TODO 6.1: check user balance
-      if (highest_bidder.balance >= auction.currentPrice) {
-        // TODO 6.2 : if sufficient reduce balance
-        highest_bidder.balance -= auction.currentPrice
+
+    const autoBidders = await Autobidding.find({
+      auctionId: auctions[i]._id,
+      userId: { $ne: highestBidder[0].userId },
+      max_bid_amount: {
+        $gte: highestBidder[0].amount + 20
       }
-      // TODO 7: Trigger transaction
-      auction.winningBid = highest_bid._id
-      await auction.save()
-      await highest_bidder.save()
-      let newNotfication = await User.findByIdAndUpdate(
-        highest_bid.userId,
-        {
-          $push: {
-            notifications: {
-              message: `You won the auction #${auction._id}`
-            }
-          }
-        },
-        { new: true }
+    }).sort({ max_bid_amount: -1 })
+
+    if (autoBidders.length !== 0) {
+      const nextBid = Math.min(
+        autoBidders[0].max_bid_amount,
+        highestBidder[0].amount + autoBidders[0].increment_amount
       )
+      const user = await User.findById(autoBidders[0].userId)
+      if (
+        highestBidder.length !== 0 &&
+        highestBidder[0].userId !== autoBidders[0].userId
+      ) {
+        // console.log(highestBidder[0].userId.toString())
+        // console.log('highest bidder', highestBidder[0])
+        const previousBidder = await User.findById(
+          highestBidder[0].userId.toString()
+        )
+        console.log('previous bidder', previousBidder)
+        console.log('id of previous bidder', previousBidder._id)
+        if (previousBidder) {
+          console.log('inside second if ', previousBidder)
+          previousBidder.lockedAmount -= highestBidder[0].amount
+          await previousBidder.save()
+          let newNotfication = await User.findByIdAndUpdate(
+            previousBidder._id,
+            {
+              $push: {
+                notifications: {
+                  message: `You've been outbid on auction #${auctions[i]._id}.`
+                }
+              }
+            },
+            { new: true }
+          )
+          console.log('previous bidder id', previousBidder._id)
+          console.log('response from notifi', newNotfication)
+          newNotfication =
+            newNotfication.notifications[
+              newNotfication.notifications.length - 1
+            ].message
 
-      newNotfication =
-        newNotfication.notifications[newNotfication.notifications.length - 1]
-          .message
-      global.io.to(highest_bid.userId.toString()).emit('notify', newNotfication)
+          console.log(previousBidder)
+          global.io
+            .to(previousBidder._id.toString())
+            .emit('notify', newNotfication)
+        }
+      }
+      if (auctions[i].status === 'ongoing') {
+        if (
+          user.balance >
+          user.lockedAmount +
+            highestBidder[0].amount +
+            autoBidders[0].increment_amount
+        ) {
+          const sd = new Date(auctions[i].startDate)
+          const ed = new Date(auctions[i].endDate)
+          if (sd <= nowUTC() < ed) {
+            const newBidding = await Bidding.create({
+              auctionId: auctions[i],
+              userId: autoBidders[0].userId,
+              amount: highestBidder[0].amount + 20
+            })
 
-      const trasnaction = await Transaction.create({
-        sellerId: auction.ownerId,
-        buyerId: highest_bid.userId,
-        price: highest_bid.amount,
-        itemId: auction.itemId,
-        date: nowUTC()
-      })
-      console.log('trasnaction', trasnaction)
+            const updatedAuction = await Auction.findByIdAndUpdate(
+              auctions[i],
+              {
+                currentPrice: newBidding.amount
+              }
+            )
+            const newBidCount = await Bidding.countDocuments(auctions[i])
+
+            global.io.to(auctions[i]).emit('newBid', {
+              auctionId: auctions[i],
+              bid: newBidding,
+              currentPrice: updatedAuction.currentPrice,
+              bidCount: newBidCount
+            })
+
+            console.log('new bidding', newBidding)
+            let newNotfication = await User.findByIdAndUpdate(
+              newBidding.userId,
+              {
+                $push: {
+                  notifications: {
+                    message: `Autobidding placed on auction #${auctions[i]._id}  @ BHD${newBidding.amount}.`
+                  }
+                }
+              },
+              { new: true }
+            )
+            newNotfication =
+              newNotfication.notifications[
+                newNotfication.notifications.length - 1
+              ].message
+            global.io
+              .to(newBidding.userId.toString())
+              .emit('notify', newNotfication)
+          }
+        }
+      }
+      user.lockedAmount += nextBid
+      await user.save()
     }
   }
-  // TODO 5: emit only ongoing auctions
-  const ongoingAuctions = await Auction.find({ status: 'ongoing' }).populate(
-    'itemId'
-  )
-
-  global.io.emit('updateAuctions', { ongoing: ongoingAuctions })
+  console.log('reached end of loop')
 }
 
-module.exports = checkAuctions
+module.exports = makeAutoBidding
+// const placeAutoBidding = async () => {
+//   // getting all autobiddings from the table
+//   const autobiddings = await Autobidding.find()
+//   console.log('autobiddings', autobiddings)
+//   let bidding = '',
+//     amount = 0,
+//     max = 0,
+//     increment_amount = 0,
+//     user = '',
+//     userDetails = 0,
+//     auction = ''
+//   for (let i = 0; i < autobiddings.length; i++) {
+//     console.log('entered for loop')
+//     bidding = await Bidding.find({ auctionId: autobiddings[i].auctionId }).sort(
+//       { amount: -1 }
+//     )
+//     amount = bidding[0].amount
+//     max = autobiddings[i].max_bid_amount
+//     increment_amount = autobiddings[i].increment_amount
+//     user = bidding[0].userId
+//     userDetails = await User.findById(user)
+//     auction = await Auction.findById(bidding[0].auctionId)
+//     if (auction.status === 'ongoing') {
+//       if (
+//         userDetails.balance >
+//         userDetails.lockedAmount + amount + increment_amount
+//       ) {
+//         const sd = new Date(auction.startDate)
+//         const ed = new Date(auction.endDate)
+//         if (sd <= nowUTC() < ed) {
+//           const newBidding = await Bidding.create({
+//             auctionId: autobiddings[i].auctionId,
+//             userId: user,
+//             amount: amount + increment_amount
+//           })
+//         }
+//       }
+//     }
+//   }
+// }
+
+// module.exports = placeAutoBidding
+
+// if auto-bidding is checked enable websocket
+// listen for biddings placed on the same auction
+// when another bidding is placed check if amount is greater than maximum bid + increment
+// if amount is less than maximum bid plus increment
+//  check if remaining balance is greater than maximum bid plus increment
+// check if time is not over
+// place bid at past maximum bit plus increment
+// repeat
+
+// const checkAutoBidding = async (auctId, id, amount, step) => {
+//   console.log('enters function')
+//   let currentPrice = parseInt(amount)
+//   let highestBidder = id
+//   let nextBidder = ''
+//   let nextBid = 0
+
+//   while (true) {
+//     console.log('loop')
+//     const autoBidders = await Autobidding.find({
+//       auctionId: auctId,
+//       userId: { $ne: highestBidder },
+//       max_bid_amount: { $gte: currentPrice + step }
+//     }).sort({ max_bid_amount: -1 })
+//     console.log(autoBidders)
+
+//     if (autoBidders.length === 0) {
+//       break
+//     }
+
+//     nextBidder = autoBidders[0]
+//     nextBid = Math.min(
+//       autoBidders[0].max_bid_amount,
+//       currentPrice + autoBidders[0].increment_amount
+//     )
+
+//     // will add to lockedAmount the bid that was placed rn
+
+//     const newBid = await Bidding.create({
+//       auctionId: auctId,
+//       userId: autoBidders[0].userId,
+//       amount: nextBid
+//     })
+
+//     currentPrice = nextBid
+//     highestBidder = autoBidders[0].userId
+//     console.log('next bid placed', newBid)
+//   }
+// }
