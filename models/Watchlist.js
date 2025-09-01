488931f12e07d3896aa0b572d045522c68d74c79
maryamalihasanebrahim <maryamalihasanebrahim@gmail.com>
2025-09-02 00:32:04 +0300

    to pull

---
+++ models/Watchlist.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const watchlistSchema = new mongoose.Schema(
+  {
+    auctionId: { type: mongoose.Schema.Types.ObjectId, ref: 'Auction' },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('Watchlist', watchlistSchema)
